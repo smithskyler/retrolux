8dbba88ed00145b2f514611c202a0887c50559e5
Skyler Smith <ssmith@izeni.com>
2017-08-18 16:09:57 -0600

    Patched HTTPClient to allow subclasses to alter the session configuration.

--- Retrolux/HTTPClient.swift
+++ Retrolux/HTTPClient.swift
@@ -9,24 +9,31 @@
 import Foundation
 
 // TODO: Add support for ignoring SSL errors.
-public class HTTPClient: NSObject, Client, URLSessionDelegate, URLSessionTaskDelegate {
-    public private(set) var session: URLSession!
-    
-    public override init() {
-        super.init()
-        let configuration = URLSessionConfiguration.default
-        session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-    }
-    
-    public func makeAsynchronousRequest(
-        request: inout URLRequest,
-        callback: @escaping (_ response: ClientResponse) -> Void
-        ) -> Task
-    {
-        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
-            let clientResponse = ClientResponse(data: data, response: response, error: error)
-            callback(clientResponse)
-        }) 
-        return HTTPTask(task: task)
-    }
+open class HTTPClient: NSObject, Client, URLSessionDelegate, URLSessionTaskDelegate {
+	public private(set) var session: URLSession!
+	
+	public override init() {
+		super.init()
+		session = URLSession(configuration: makeSessionConfiguration(), delegate: self, delegateQueue: nil)
+	}
+	
+	public func makeAsynchronousRequest(
+		request: inout URLRequest,
+		callback: @escaping (_ response: ClientResponse) -> Void
+		) -> Task
+	{
+		request.cachePolicy = session.configuration.requestCachePolicy
+		request.timeoutInterval = session.configuration.timeoutIntervalForRequest
+		let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+			let clientResponse = ClientResponse(data: data, response: response, error: error)
+			callback(clientResponse)
+		})
+		return HTTPTask(task: task)
+	}
+	
+	/// Subclasses can override this method to provide configuration options during init
+	/// (changing a configuration's properties once the session has been created has no effect)
+	public func makeSessionConfiguration() -> URLSessionConfiguration {
+		return URLSessionConfiguration.default
+	}
 }
