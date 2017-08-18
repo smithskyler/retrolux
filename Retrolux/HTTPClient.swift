a4989e151bdb695238ee5a64f48974198ad6e3df
Skyler Smith <ssmith@izeni.com>
2017-08-18 16:11:09 -0600

    Reverted patch to indent with spaces, as per project standard.

--- Retrolux/HTTPClient.swift
+++ Retrolux/HTTPClient.swift
@@ -10,30 +10,30 @@ import Foundation
 
 // TODO: Add support for ignoring SSL errors.
 open class HTTPClient: NSObject, Client, URLSessionDelegate, URLSessionTaskDelegate {
-	public private(set) var session: URLSession!
-	
-	public override init() {
-		super.init()
-		session = URLSession(configuration: makeSessionConfiguration(), delegate: self, delegateQueue: nil)
-	}
-	
-	public func makeAsynchronousRequest(
-		request: inout URLRequest,
-		callback: @escaping (_ response: ClientResponse) -> Void
-		) -> Task
-	{
-		request.cachePolicy = session.configuration.requestCachePolicy
-		request.timeoutInterval = session.configuration.timeoutIntervalForRequest
-		let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
-			let clientResponse = ClientResponse(data: data, response: response, error: error)
-			callback(clientResponse)
-		})
-		return HTTPTask(task: task)
-	}
-	
-	/// Subclasses can override this method to provide configuration options during init
-	/// (changing a configuration's properties once the session has been created has no effect)
-	public func makeSessionConfiguration() -> URLSessionConfiguration {
-		return URLSessionConfiguration.default
-	}
+    public private(set) var session: URLSession!
+    
+    public override init() {
+        super.init()
+        session = URLSession(configuration: makeSessionConfiguration(), delegate: self, delegateQueue: nil)
+    }
+    
+    public func makeAsynchronousRequest(
+        request: inout URLRequest,
+        callback: @escaping (_ response: ClientResponse) -> Void
+        ) -> Task
+    {
+        request.cachePolicy = session.configuration.requestCachePolicy
+        request.timeoutInterval = session.configuration.timeoutIntervalForRequest
+        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+            let clientResponse = ClientResponse(data: data, response: response, error: error)
+            callback(clientResponse)
+        })
+        return HTTPTask(task: task)
+    }
+    
+    /// Subclasses can override this method to provide configuration options during init
+    /// (changing a configuration's properties once the session has been created has no effect)
+    public func makeSessionConfiguration() -> URLSessionConfiguration {
+        return URLSessionConfiguration.default
+    }
 }
